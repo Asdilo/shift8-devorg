aea0b1ef62a00cfa6033c7bdc560383f26c688c6
Asterisk Loftis <asterisk@asteriskloftis.com>
2019-06-17 16:34:20 -0700

    Adding lwc inputs

--- shift8/shift8/force-app/main/default/lwc/inputListBuilder/inputListBuilder.js
+++ shift8/shift8/force-app/main/default/lwc/inputListBuilder/inputListBuilder.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class InputListBuilder extends LightningElement {}
+export default class InputListBuilder extends LightningElement {
+  @track value = "onceDaily";
+
+  get options() {
+    return [
+      { label: "As Needed", value: "asNeeded" },
+      { label: "Once Daily", value: "onceDaily" },
+      { label: "Once Weekly", value: "onceWeekly" }
+    ];
+  }
+
+  handleChange(event) {
+    this.value = event.detail.value;
+  }
+}

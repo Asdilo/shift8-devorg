a864c1a1557437d954907d2962d55e20d18b9d54
Asterisk Loftis <asterisk@asteriskloftis.com>
2019-07-02 09:22:42 -0700

    Removed duplicates

--- shift8/shift8/force-app/main/default/lwc/inputListBuilder/inputListBuilder.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from "lwc";
-
-export default class InputListBuilder extends LightningElement {
-  @api title = "Custom Input List Builder";
-  @api listType = "Choose";
-  @api showEmpty = "false";
-}
--- shift8/shift8/force-app/main/default/lwc/listBuilderEmpty/listBuilderEmpty.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ListBuilderEmpty extends LightningElement {}

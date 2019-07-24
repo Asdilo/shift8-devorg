ab4854b773cc74ff8d8dbec315a38c03782cd6d1
Asterisk Loftis <asterisk@asteriskloftis.com>
2019-07-23 22:21:40 -0700

    Updating with a few ways to target the DOm despite the Shadowness

--- force-app/main/default/lwc/cssAnimations/cssAnimations.js
+++ force-app/main/default/lwc/cssAnimations/cssAnimations.js
@@ -1,10 +1,35 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class CssAnimations extends LightningElement {
 
-    toggle() {
-        var myEl = this.template.querySelector(".toggler");
-        myEl.classList.toggle("slds-is-collapsed");
-        this.isSelected = !this.isSelected;
-      }
-}
+  renderedCallback() {
+//target your elements with query selection
+    var myEls = this.template.querySelectorAll(".slds-accordion__content");
+    var i;
+
+    if (this.hasRendered) return;
+    this.hasRendered = true;
+
+    //loop through node list retunred from querySellectorAll
+    for (i = 0; i < myEls.length; i++) {
+      myEls[i].classList.add("poop");
+      myEls[i].style.backgroundColor = "red";
+    }
+//use this for standard components
+    const style = document.createElement('style');
+    style.innerText = `
+    .slds-is-open>.slds-accordion__content {
+      -webkit-transition: height 2s, opacity 2s; /* Safari prior 6.1 */
+      transition: height 2s, opacity 2s;
+  }
+    `;
+    this.template.querySelector('lightning-card').appendChild(style);
+  }
+  
+
+  toggle() {
+    var myEl = this.template.querySelector(".toggler");
+    myEl.classList.toggle("slds-is-collapsed");
+    this.isSelected = !this.isSelected;
+  }
+  }

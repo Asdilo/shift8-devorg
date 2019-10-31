e2ee6ba634f40dca5e6d28009f72338fb5951bc3
Asterisk Loftis <asterisk@asteriskloftis.com>
2019-10-31 14:00:42 -0700

    Fake data added

--- force-app/main/default/lwc/tileList/tileList.js
+++ force-app/main/default/lwc/tileList/tileList.js
@@ -1,7 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
+const results = [
+    {uniqueId:'A',experimentName:'Cool Name Here Yall', experimenLink: 'https://www.optimizely.com', variationName:'CHeeseburger', dateTime: 1547250828000},
+
+    {uniqueId:'B',experimentName:'Cool Name Here Yall', experimenLink: 'https://www.optimizely.com', variationName:'CHeeseburger', dateTime: 1547250828000},
+];
 export default class TileList extends LightningElement {
     @api title;
     @api icon;
+
+    @track results = results;
 
 }

46f805afdd075d2354a3d46ff79768906b09e7f4
Asterisk Loftis <asterisk@asteriskloftis.com>
2019-11-01 11:05:59 -0700

    UI deve complete for Optimizely exeriment list

--- force-app/main/default/lwc/tileList/tileList.js
+++ force-app/main/default/lwc/tileList/tileList.js
@@ -1,13 +1,43 @@
 import { LightningElement, api, track } from 'lwc';
+import OptimizelyIcon from '@salesforce/resourceUrl/OptimizelyIcon';
 
 const results = [
-    {uniqueId:'A',experimentName:'Cool Name Here Yall', experimentLink: 'https://www.optimizely.com', variationName:'CHeeseburger', dateTime: 1547250828000},
-    {uniqueId:'B',experimentName:'Cool Name Here Yall', experimentLink: 'https://www.optimizely.com', variationName:'CHeeseburger', dateTime: 1547250828000},
+  {
+    uniqueId: "A",
+    experimentName: "Streamline Checkout Flow",
+    variationName: "Variation URL Link Here",
+    variationLink: "https://www.optimizely.com/",
+    dateTime: 1547250828000,
+    experimentLink: "https://optimizely-df19demo-dev-ed.lightning.force.com/lightning/r/Experiment__c/a033k00000QHiqjAAD/view?ws=%2Flightning%2Fr%2FProject__c%2Fa083k00001fMbx8AAC%2Fview"
+  },
+  {
+    uniqueId: "B",
+    experimentName: "Checkout CTA Test",
+    variationName: "Variation URL Link Here",
+    variationLink: "https://www.optimizely.com/",
+    dateTime: 1547250828000,
+    experimentLink:"https://optimizely-df19demo-dev-ed.lightning.force.com/lightning/r/Experiment__c/a033k00000QHiqiAAD/view?ws=%2Flightning%2Fr%2FProject__c%2Fa083k00001fMbx8AAC%2Fview"
+  },
+  {
+    uniqueId: "C",
+    experimentName: "Cyber Monday Discounts",
+    variationName: "Variation URL Link Here",
+    variationLink: "https://www.optimizely.com/",
+    dateTime: 1547250828000,
+    experimentLink:"https://optimizely-df19demo-dev-ed.lightning.force.com/lightning/r/Experiment__c/a033k00000QHiqnAAD/view?ws=%2Flightning%2Fr%2FProject__c%2Fa083k00001fMbx8AAC%2Fview"
+  },
+  {
+    uniqueId: "D",
+    experimentName: "Navigation Image Test",
+    variationName: "Variation URL Link Here",
+    variationLink: "https://www.optimizely.com/",
+    dateTime: 1547250828000,
+    experimentLink:"https://optimizely-df19demo-dev-ed.lightning.force.com/lightning/r/Experiment__c/a033k00000QHiqsAAD/view?ws=%2Flightning%2Fr%2FProject__c%2Fa083k00001fMbx8AAC%2Fview"
+  }
 ];
 export default class TileList extends LightningElement {
     @api title;
-    @api icon;
-
     @track results = results;
+    optIcon = OptimizelyIcon + '#optimizelyIcon';
 
 }

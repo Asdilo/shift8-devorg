285c3a3da089d3993f3a2b50c1ae8da3af558223
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-02-21 20:44:52 -0700

    More updates to record edit form

--- force-app/main/default/lwc/recordForm/recordForm.js
+++ force-app/main/default/lwc/recordForm/recordForm.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class RecordForm extends LightningElement {}
+export default class RecordForm extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+    @api fields = ["Type", "StageName", "Amount"];
+    @api columns = 3;
+}

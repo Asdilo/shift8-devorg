62db461ccf9f7eb7f0577c89f2f00e6d3c7aea3d
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-02-22 13:47:24 -0700

    Added in final touches and comments

--- force-app/main/default/lwc/recordForm/recordForm.js
+++ force-app/main/default/lwc/recordForm/recordForm.js
@@ -3,11 +3,16 @@ import { LightningElement, api, track } from "lwc";
 export default class RecordForm extends LightningElement {
   @api recordId;
   @api objectApiName;
-  @api fields;
-  @api columns;
+  @api fields; //User enter fields via App Builder
+  @api columns; //User enter number of comumns via App Builder
   @track fieldList;
+
+  //The connectedCallback runs autonatically when lwc is inserted into the DOM
   connectedCallback() {
     var userFields = this.fields;
-    this.fieldList = userFields.split(",");
+    //The following statement takes the user entered fields splits them into an array
+    //Then takes each array item and maps it back to an array trimmed using a arrowed function for the ES6 swagger
+    //Finally, assign those values to thefieldList tracked variable 
+    this.fieldList = userFields.split(",").map(item => item.trim()); 
   }
 }

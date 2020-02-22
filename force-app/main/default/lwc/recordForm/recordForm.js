be50484144e8813b056ea242465323f650e0d2b0
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-02-22 00:09:50 -0700

    Got it working!

--- force-app/main/default/lwc/recordForm/recordForm.js
+++ force-app/main/default/lwc/recordForm/recordForm.js
@@ -1,9 +1,13 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from "lwc";
 
 export default class RecordForm extends LightningElement {
-
-    @api recordId;
-    @api objectApiName;
-    @api fields = ["Type", "StageName", "Amount"];
-    @api columns = 3;
+  @api recordId;
+  @api objectApiName;
+  @api fields;
+  @api columns;
+  @track fieldList;
+  connectedCallback() {
+    var userFields = this.fields;
+    this.fieldList = userFields.split(",");
+  }
 }

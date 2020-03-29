fd06156836feef1029f33cce9698fc6b8c306f8d
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-03-29 14:06:19 -0700

    updates to POC

--- force-app/main/default/lwc/pocIqviaSVP/pocIqviaSVP.js
+++ force-app/main/default/lwc/pocIqviaSVP/pocIqviaSVP.js
@@ -1,5 +1,20 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class PocIqviaSVP extends LightningElement {
     @api title;
+    @api icon = "standard:opportunity";
+    @api classes = "";
+    @track radioVal = '';
+
+    get radioOpt() {
+        return [
+            { label: 'Sun', value: 'sunday' },
+            { label: 'Mon', value: 'monday' },
+            { label: 'Tue', value: 'tuesday' },
+            { label: 'Wed', value: 'wednesday' },
+            { label: 'Thu', value: 'thursday' },
+            { label: 'Fri', value: 'friday' },
+            { label: 'Sat', value: 'saturday' },
+        ];
+    }
 }

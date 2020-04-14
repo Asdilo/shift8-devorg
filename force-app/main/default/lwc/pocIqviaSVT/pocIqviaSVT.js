21935dec5d04b2ceda052b7aefd75898516f91a8
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-04-14 06:48:21 -0700

    80% done with POC

--- force-app/main/default/lwc/pocIqviaSVT/pocIqviaSVT.js
+++ force-app/main/default/lwc/pocIqviaSVT/pocIqviaSVT.js
@@ -10,19 +10,14 @@ const actions = [
 
 const columns = [
   {
-    type: "number",
-    fieldName: "subjectId",
-    label: "Subject ID",
+    type: "text",
+    fieldName: "items",
+    label: "Verification Item",
   },
   {
     type: "text",
-    fieldName: "status",
-    label: "Status"
-  },
-  {
-    type: "date",
-    fieldName: "visit",
-    label: "Visit"
+    fieldName: "completed",
+    label: "Completion Status"
   },
   {
     type: "action",
@@ -31,170 +26,402 @@ const columns = [
 ];
 
 const data = [{
-    "subjectId": 6001,
-    "status": "Randomized",
-    "visit": [
-      {
-        "Date": "06/17/2019",
-        "verification": [
-          {
-            "items": "Account Representative II",
-            "completed": true
-          },
-          {
-            "items": "VP Product Management",
-            "completed": false
-          },
-          {
-            "items": "Geologist I",
-            "completed": false
-          },
-          {
-            "items": "Office Assistant I",
-            "completed": true
-          }
-        ]
-      }
-    ]
-  }, {
-    "subjectId": 1259,
-    "status": "Screened",
-    "visit": [
-      {
-        "Date": "01/22/2020",
-        "verification": [
-          {
-            "items": "Quality Engineer",
-            "completed": true
-          },
-          {
-            "items": "Web Developer II",
-            "completed": true
-          },
-          {
-            "items": "Computer Systems Analyst IV",
-            "completed": false
-          }
-        ]
-      },
-      {
-        "Date": "12/30/2019",
-        "verification": [
-          {
-            "items": "Accounting Assistant II",
-            "completed": false
-          },
-          {
-            "items": "Internal Auditor",
-            "completed": false
-          },
-          {
-            "items": "Senior Cost Accountant",
-            "completed": true
-          }
-        ]
-      }
-    ]
-  }, {
-    "subjectId": 4805,
-    "status": "Completed",
-    "visit": [
-      {
-        "Date": "07/28/2019",
-        "verification": [
-          {
-            "items": "Accounting Assistant I",
-            "completed": true
-          },
-          {
-            "items": "Graphic Designer",
-            "completed": false
-          },
-          {
-            "items": "Project Manager",
-            "completed": false
-          },
-          {
-            "items": "Registered Nurse",
-            "completed": true
-          }
-        ]
-      },
-      {
-        "Date": "07/22/2019",
-        "verification": [
-          {
-            "items": "Safety Technician IV",
-            "completed": true
-          },
-          {
-            "items": "Accountant I",
-            "completed": true
-          }
-        ]
-      }
-    ]
-  }, {
-    "subjectId": 1402,
-    "status": "Screened",
-    "visit": [
-      {
-        "Date": "03/04/2020",
-        "verification": [
-          {
-            "items": "Pharmacist",
-            "completed": false
-          },
-          {
-            "items": "Financial Analyst",
-            "completed": true
-          },
-          {
-            "items": "Product Engineer",
-            "completed": true
-          },
-          {
-            "items": "VP Accounting",
-            "completed": false
-          },
-          {
-            "items": "Compensation Analyst",
-            "completed": false
-          }
-        ]
-      },
-      {
-        "Date": "06/01/2019",
-        "verification": [
-          {
-            "items": "Help Desk Technician",
-            "completed": false
-          }
-        ]
-      }
-    ]
-  }, {
-    "subjectId": 5669,
-    "status": "Discontinued",
-    "visit": [
-      {
-        "Date": "03/08/2020",
-        "verification": [
-          {
-            "items": "Administrative Officer",
-            "completed": false
-          }
-        ]
-      }
-    ]
-  }];
+  "subjectId": 6341,
+  "status": "Discontinued",
+  "label": "6341 - Discontinued",
+  "visit": [
+    {
+      "date": "05/11/2019",
+      "verification": [
+        {
+          "items": "Physical Exam Documentation",
+          "completed": false
+        },
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "Laboratory Results",
+          "completed": true
+        },
+        {
+          "items": "X-Rays",
+          "completed": false
+        },
+        {
+          "items": "Laboratory Results",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "07/22/2019",
+      "verification": [
+        {
+          "items": "Laboratory Results",
+          "completed": false
+        },
+        {
+          "items": "Informed Consent",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "02/18/2020",
+      "verification": [
+        {
+          "items": "Physical Exam Documentation",
+          "completed": true
+        },
+        {
+          "items": "Physical Exam Documentation",
+          "completed": true
+        },
+        {
+          "items": "Laboratory Results",
+          "completed": true
+        },
+        {
+          "items": "Hearing Test",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "07/11/2019",
+      "verification": [
+        {
+          "items": "Endoscopy",
+          "completed": false
+        },
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "EKG (Electrocardiogram)",
+          "completed": true
+        },
+        {
+          "items": "Vision Test",
+          "completed": false
+        }
+      ]
+    }
+  ]
+}, {
+  "subjectId": 6854,
+  "status": "Completed",
+  "label": "6854 - Completed",
+  "visit": [
+    {
+      "date": "12/12/2019",
+      "verification": [
+        {
+          "items": "Laboratory Results",
+          "completed": true
+        },
+        {
+          "items": "Informed Consent",
+          "completed": true
+        }
+      ]
+    },
+    {
+      "date": "11/30/2019",
+      "verification": [
+        {
+          "items": "X-Rays",
+          "completed": false
+        },
+        {
+          "items": "X-Rays",
+          "completed": true
+        },
+        {
+          "items": "PFT (Pulmonary Function Test)",
+          "completed": true
+        },
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "CT(Computed Tomography)",
+          "completed": false
+        },
+        {
+          "items": "PFT (Pulmonary Function Test)",
+          "completed": false
+        },
+        {
+          "items": "Physical Exam Documentation",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        },
+        {
+          "items": "Vitals",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        }
+      ]
+    }
+  ]
+}, {
+  "subjectId": 1664,
+  "status": "Screened",
+  "label": "1664 - Screened",
+  "visit": [
+    {
+      "date": "05/27/2019",
+      "verification": [
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "PFT (Pulmonary Function Test)",
+          "completed": false
+        },
+        {
+          "items": "Informed Consent",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": false
+        },
+        {
+          "items": "X-Rays",
+          "completed": false
+        },
+        {
+          "items": "Endoscopy",
+          "completed": false
+        },
+        {
+          "items": "Vitals",
+          "completed": false
+        },
+        {
+          "items": "Hearing Test",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": false
+        },
+        {
+          "items": "Informed Consent",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "03/09/2020",
+      "verification": [
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        },
+        {
+          "items": "CT(Computed Tomography)",
+          "completed": false
+        },
+        {
+          "items": "Hearing Test",
+          "completed": true
+        },
+        {
+          "items": "Endoscopy",
+          "completed": true
+        },
+        {
+          "items": "PFT (Pulmonary Function Test)",
+          "completed": false
+        }
+      ]
+    }
+  ]
+}, {
+  "subjectId": 2654,
+  "status": "Completed",
+  "label": "2654 - Completed",
+  "visit": [
+    {
+      "date": "09/01/2019",
+      "verification": [
+        {
+          "items": "X-Rays",
+          "completed": true
+        },
+        {
+          "items": "Vitals",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        }
+      ]
+    },
+    {
+      "date": "05/10/2019",
+      "verification": [
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": true
+        },
+        {
+          "items": "Endoscopy",
+          "completed": false
+        },
+        {
+          "items": "Informed Consent",
+          "completed": true
+        },
+        {
+          "items": "Physical Exam Documentation",
+          "completed": false
+        },
+        {
+          "items": "X-Rays",
+          "completed": true
+        },
+        {
+          "items": "Physical Exam Documentation",
+          "completed": true
+        },
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "PFT (Pulmonary Function Test)",
+          "completed": false
+        },
+        {
+          "items": "X-Rays",
+          "completed": true
+        }
+      ]
+    },
+    {
+      "date": "12/19/2019",
+      "verification": [
+        {
+          "items": "Laboratory Results",
+          "completed": false
+        },
+        {
+          "items": "X-Rays",
+          "completed": true
+        },
+        {
+          "items": "Vision Test",
+          "completed": true
+        },
+        {
+          "items": "Hearing Test",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "09/28/2019",
+      "verification": [
+        {
+          "items": "Vitals",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "02/03/2020",
+      "verification": [
+        {
+          "items": "Endoscopy",
+          "completed": true
+        }
+      ]
+    }
+  ]
+}, {
+  "subjectId": 9069,
+  "status": "Discontinued",
+  "label": "9069 - Discontinued",
+  "visit": [
+    {
+      "date": "05/20/2019",
+      "verification": [
+        {
+          "items": "Informed Consent",
+          "completed": false
+        },
+        {
+          "items": "Informed Consent",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": false
+        },
+        {
+          "items": "EEG (Electroencephalogram)",
+          "completed": false
+        }
+      ]
+    },
+    {
+      "date": "09/15/2019",
+      "verification": [
+        {
+          "items": "Hearing Test",
+          "completed": false
+        },
+        {
+          "items": "Endoscopy",
+          "completed": false
+        },
+        {
+          "items": "Laboratory Results",
+          "completed": false
+        },
+        {
+          "items": "EKG (Electrocardiogram)",
+          "completed": true
+        },
+        {
+          "items": "Laboratory Results",
+          "completed": false
+        }
+      ]
+    }
+  ]
+}];
 
 
 export default class PocIqviaSVT extends LightningElement {
   @api title;
   @api icon = "standard:user";
   @api flexipageRegionWidth;
-  @track recordNum = 4;
+  @track recordNum = data.length;
   @track svtColumns = columns;
   @track svtData = data;
   @track activeSubjects = [];

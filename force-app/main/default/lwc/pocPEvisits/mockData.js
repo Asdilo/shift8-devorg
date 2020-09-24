7c82a98a5ebc60e64bb739edd5bd2d7a5bb885a4
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-09-23 18:03:44 -0700

    Adding interactivity

--- force-app/main/default/lwc/pocPEvisits/mockData.js
+++ force-app/main/default/lwc/pocPEvisits/mockData.js
@@ -1,3 +1,194 @@
-const pocData = [1,2,3,4,5,6]
+const upcomingData = [{
+    "id": 1,
+    "name": "Yellow-billed hornbill",
+    "date_time": "2020-12-29T21:31:34Z",
+    "location": "49711 Riverside Pass",
+    "overview": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.",
+    "details": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
+    "scheduled": false,
+    "type": "Onsite Location",
+    "icon": "standard:address",
+    "study_team": [
+      {
+        "name": "Otto Sherewood",
+        "role": "Structural Engineer",
+        "phone": "(512) 6878417",
+        "image": "https://robohash.org/quicupiditateharum.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      },
+      {
+        "name": "Theressa Hillborne",
+        "role": "Budget/Accounting Analyst III",
+        "phone": "(279) 7107981",
+        "image": "https://robohash.org/nonveritatisadipisci.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      },
+      {
+        "name": "Giavani Philipsson",
+        "role": "Civil Engineer",
+        "phone": "(101) 1915181",
+        "image": "https://robohash.org/sedcupiditateaut.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      }
+    ]
+  }, {
+    "id": 2,
+    "name": "Buttermilk snake",
+    "date_time": "2020-10-22T18:11:13Z",
+    "location": "8629 Utah Plaza",
+    "overview": "Praesent blandit lacinia erat.",
+    "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    "scheduled": true,
+    "type": "Video Call",
+    "icon": "standard:employee_asset",
+    "study_team": [
+      {
+        "name": "Ernie Moralas",
+        "role": "Senior Sales Associate",
+        "phone": "(604) 6284567",
+        "image": "https://robohash.org/enimuttenetur.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      }
+    ]
+  }, {
+    "id": 3,
+    "name": "Brown pelican",
+    "date_time": "2020-11-16T03:08:49Z",
+    "location": "8458 Autumn Leaf Parkway",
+    "overview": "Duis ac nibh.",
+    "details": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    "scheduled": true,
+    "type": "Phone Call",
+    "icon": "standard:voice_call",
+    "study_team": [
+      {
+        "name": "Lin Linneman",
+        "role": "Programmer Analyst III",
+        "phone": "(682) 6575304",
+        "image": "https://robohash.org/nammolestiaequo.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar2.jpg"
+      },
+      {
+        "name": "Miguelita McAmish",
+        "role": "Senior Cost Accountant",
+        "phone": "(435) 6458288",
+        "image": "https://robohash.org/verorepellatquae.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      },
+      {
+        "name": "Levey Barrowcliffe",
+        "role": "Marketing Manager",
+        "phone": "(623) 1866935",
+        "image": "https://robohash.org/sedaliquamconsequatur.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      }
+    ]
+  }, {
+    "id": 4,
+    "name": "Northern elephant seal",
+    "date_time": "2020-12-14T22:44:49Z",
+    "location": "538 Packers Way",
+    "overview": "In congue. Etiam justo.",
+    "details": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
+    "scheduled": false,
+    "type": "Onsite Location",
+    "icon": "standard:address",
+    "study_team": [
+      {
+        "name": "Heindrick Bassill",
+        "role": "Office Assistant I",
+        "phone": "(345) 9339301",
+        "image": "https://robohash.org/errorexplicabominima.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      },
+      {
+        "name": "Jody Gaunson",
+        "role": "Chemical Engineer",
+        "phone": "(241) 2809051",
+        "image": "https://robohash.org/doloresuntet.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      },
+      {
+        "name": "Rowan Screaton",
+        "role": "Senior Financial Analyst",
+        "phone": "(827) 6721749",
+        "image": "https://robohash.org/ipsamreiciendisiusto.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      }
+    ]
+  }];
 
-export { pocData};
+  const pastData = [{
+    "id": 1,
+    "name": "Porcupine, indian",
+    "date_time": "2020-11-18T06:10:28Z",
+    "location": "073 Leroy Terrace",
+    "overview": "Pellentesque ultrices mattis odio.",
+    "details": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    "scheduled": false,
+    "type": "Onsite Location",
+    "icon": "standard:address",
+    "study_team": [
+      {
+        "name": "Brewster Bouzek",
+        "role": "Recruiter",
+        "phone": "(563) 6005597",
+        "image": "https://robohash.org/nemoexplicaborerum.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar1.jpg"
+      },
+      {
+        "name": "Artie Veale",
+        "role": "Quality Control Specialist",
+        "phone": "(759) 4048773",
+        "image": "https://robohash.org/impeditasperioresnihil.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      }
+    ]
+  }, {
+    "id": 2,
+    "name": "Eleven-banded armadillo (unidentified)",
+    "date_time": "2020-10-02T19:25:01Z",
+    "location": "16518 Doe Crossing Street",
+    "overview": "Duis at velit eu est congue elementum. In hac habitasse platea dictumst.",
+    "details": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    "scheduled": false,
+    "type": "Video Call",
+    "icon": "standard:employee_asset",
+    "study_team": [
+      {
+        "name": "Costanza Greengrass",
+        "role": "Research Associate",
+        "phone": "(173) 3624077",
+        "image": "https://robohash.org/omnisautut.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar2.jpg"
+      }
+    ]
+  }, {
+    "id": 3,
+    "name": "Phascogale, brush-tailed",
+    "date_time": "2020-11-16T18:25:56Z",
+    "location": "31 Montana Place",
+    "overview": "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
+    "details": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.",
+    "scheduled": false,
+    "type": "Phone Call",
+    "icon": "standard:voice_call",
+    "study_team": [
+      {
+        "name": "Ruthy Haxbie",
+        "role": "Product Engineer",
+        "phone": "(918) 2765545",
+        "image": "https://robohash.org/pariaturvoluptasrerum.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      },
+      {
+        "name": "Alexandrina Duffy",
+        "role": "Developer I",
+        "phone": "(724) 4762752",
+        "image": "https://robohash.org/maioresdoloresminima.jpg?size=100x100&set=set1",
+        "avatar": "https://www.lightningdesignsystem.com/assets/images/avatar3.jpg"
+      }
+    ]
+  }];
+
+export { upcomingData, pastData};
--- force-app/main/default/lwc/pocPEvisits/pocPEvisits.js
+++ force-app/main/default/lwc/pocPEvisits/pocPEvisits.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
-import {pocData} from './mockData'
+import {upcomingData} from './mockData'
+import {pastData} from './mockData'
+
 
 export default class PocPEvisits extends LightningElement {
 
@@ -7,6 +9,37 @@ export default class PocPEvisits extends LightningElement {
     @api title;
     @api cta;
     @api btntxt;
-    @track data = pocData;
+    @api height;
+    //@track style = '"height: ' + PocPEvisits.height +'px;"'
+    @track data = upcomingData;
+    @track past = pastData;
+    @track filterVals = 'all';
+    @track isModalOpen = false; //Modal state
+    @track selectedVisit = '1'; //State of which visit is selected
+
+
+    get options() {
+        return [
+            { label: 'Show All', value: 'all' },
+            { label: 'Show Scheduled', value: 'scheduled' },
+            { label: 'Show Unscheduled', value: 'unscheduled' },
+        ];
+    }
+    select(event) {
+        // 
+        var targetKey = event.currentTarget.getAttribute("id");
+        this.selectedVisit = targetKey;
+        console.log(this.selectedVisit);
+    }
+
+    modalOpen() {
+        // Open modal
+        this.isModalOpen = true;
+    }
+  
+    modalClose() {
+        // Close modal
+        this.isModalOpen = false;
+    }
 
 }

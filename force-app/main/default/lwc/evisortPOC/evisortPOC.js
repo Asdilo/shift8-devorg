15d035f9995f50fcc4876af9f19699dc8cdb11a3
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-10-20 22:06:47 -0700

    Updates to Evisort POC

--- force-app/main/default/lwc/evisortPOC/evisortPOC.js
+++ force-app/main/default/lwc/evisortPOC/evisortPOC.js
@@ -1,6 +1,18 @@
 import { LightningElement, api } from 'lwc';
+import Evisort_Resources from '@salesforce/resourceUrl/Evisort';
 
 export default class EvisortPOC extends LightningElement {
     @api title;
+
+     logoPngUrl = Evisort_Resources + '/evisortResources/logo.png';
+     logoJpgUrl = Evisort_Resources + '/evisortResources/logo.jpg';
+     logoSvgUrl = Evisort_Resources + '/evisortResources/evisortLogo.svg#colorLogo';
+     iconSvgUrl = Evisort_Resources + '/evisortResources/evisortIcon.svg#colorIcon';
+
+     isValid = false;
+
+     handleValid() {
+         this.isValid = !this.isValid;
+     }
 
 }

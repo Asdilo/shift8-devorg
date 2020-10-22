baa20008fda1465fc483c0d763952e48b88f4665
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-10-21 22:42:16 -0700

    Updates

--- force-app/main/default/lwc/evisortPOC/evisortPOC.js
+++ force-app/main/default/lwc/evisortPOC/evisortPOC.js
@@ -1,8 +1,38 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import Evisort_Resources from '@salesforce/resourceUrl/Evisort';
+import { mockData } from "./mockData";
 
+const columns = [
+    {
+      type: "text",
+      fieldName: "label",
+      label: "Label",
+      sortable: true
+    },
+    {
+        type: "text",
+        fieldName: "type",
+        label: "Type",
+        sortable: true
+      },
+      {
+        type: "boolean",
+        fieldName: "active",
+        label: "Active",
+        sortable: true,
+        editable: true
+    },
+    {
+      type: "date",
+      fieldName: "created",
+      label: "Created Date",
+      sortable: true
+    }
+  ];
 export default class EvisortPOC extends LightningElement {
     @api title;
+    @track data = mockData;
+    @track columns = columns;
 
      logoPngUrl = Evisort_Resources + '/evisortResources/logo.png';
      logoJpgUrl = Evisort_Resources + '/evisortResources/logo.jpg';
---
+++ force-app/main/default/lwc/evisortPOC/mockData.js
@@ -0,0 +1,273 @@
+const mockData = [{
+    "id": 1,
+    "label": "lectus pellentesque",
+    "type": "String",
+    "active": true,
+    "created": "2020-09-30T05:49:40Z"
+  }, {
+    "id": 2,
+    "label": "eget eleifend luctus",
+    "type": "Boolean",
+    "active": false,
+    "created": "2020-10-03T08:36:02Z"
+  }, {
+    "id": 3,
+    "label": "odio justo",
+    "type": "Date Time",
+    "active": false,
+    "created": "2020-09-14T02:04:29Z"
+  }, {
+    "id": 4,
+    "label": "felis sed",
+    "type": "Boolean",
+    "active": true,
+    "created": "2020-07-12T19:56:18Z"
+  }, {
+    "id": 5,
+    "label": "nam",
+    "type": "Boolean",
+    "active": true,
+    "created": "2020-09-24T14:49:34Z"
+  }, {
+    "id": 6,
+    "label": "amet eleifend",
+    "type": "Date Time",
+    "active": true,
+    "created": "2020-10-14T05:08:26Z"
+  }, {
+    "id": 7,
+    "label": "platea dictumst",
+    "type": "Date Time",
+    "active": false,
+    "created": "2020-06-11T07:52:31Z"
+  }, {
+    "id": 8,
+    "label": "aliquam convallis",
+    "type": "Date Time",
+    "active": true,
+    "created": "2020-05-18T20:47:26Z"
+  }, {
+    "id": 9,
+    "label": "donec diam neque",
+    "type": "Boolean",
+    "active": false,
+    "created": "2020-06-07T12:13:00Z"
+  }, {
+    "id": 10,
+    "label": "lobortis ligula sit amet",
+    "type": "Number",
+    "active": false,
+    "created": "2019-12-03T07:39:04Z"
+  }, {
+    "id": 11,
+    "label": "eget rutrum at lorem",
+    "type": "String",
+    "active": true,
+    "created": "2019-11-29T18:37:34Z"
+  }, {
+    "id": 12,
+    "label": "at nibh",
+    "type": "String",
+    "active": false,
+    "created": "2020-03-26T18:13:57Z"
+  }, {
+    "id": 13,
+    "label": "lorem ipsum dolor",
+    "type": "Number",
+    "active": false,
+    "created": "2020-02-20T16:13:35Z"
+  }, {
+    "id": 14,
+    "label": "eget orci vehicula condimentum",
+    "type": "Date Time",
+    "active": true,
+    "created": "2020-04-12T07:31:49Z"
+  }, {
+    "id": 15,
+    "label": "nisi nam",
+    "type": "Single Select",
+    "active": true,
+    "created": "2020-03-24T05:16:54Z"
+  }, {
+    "id": 16,
+    "label": "donec semper sapien",
+    "type": "Boolean",
+    "active": true,
+    "created": "2020-03-11T10:41:38Z"
+  }, {
+    "id": 17,
+    "label": "id mauris vulputate",
+    "type": "Date Time",
+    "active": true,
+    "created": "2019-10-25T09:09:20Z"
+  }, {
+    "id": 18,
+    "label": "non velit donec",
+    "type": "Single Select",
+    "active": false,
+    "created": "2020-10-05T00:49:21Z"
+  }, {
+    "id": 19,
+    "label": "a libero nam dui",
+    "type": "Single Select",
+    "active": false,
+    "created": "2020-06-25T07:20:52Z"
+  }, {
+    "id": 20,
+    "label": "mollis",
+    "type": "Multi Select",
+    "active": false,
+    "created": "2019-11-05T13:03:44Z"
+  }, {
+    "id": 21,
+    "label": "magna vulputate",
+    "type": "Single Select",
+    "active": false,
+    "created": "2020-06-13T07:26:01Z"
+  }, {
+    "id": 22,
+    "label": "nibh fusce lacus purus",
+    "type": "Number",
+    "active": true,
+    "created": "2019-12-05T11:00:55Z"
+  }, {
+    "id": 23,
+    "label": "turpis enim blandit mi",
+    "type": "Date Time",
+    "active": false,
+    "created": "2019-12-20T18:04:40Z"
+  }, {
+    "id": 24,
+    "label": "pellentesque ultrices mattis odio",
+    "type": "String",
+    "active": false,
+    "created": "2020-10-14T17:09:41Z"
+  }, {
+    "id": 25,
+    "label": "lacinia aenean sit",
+    "type": "String",
+    "active": true,
+    "created": "2020-09-30T08:24:20Z"
+  }, {
+    "id": 26,
+    "label": "lacinia erat",
+    "type": "String",
+    "active": false,
+    "created": "2020-10-12T05:26:36Z"
+  }, {
+    "id": 27,
+    "label": "faucibus orci",
+    "type": "Number",
+    "active": true,
+    "created": "2020-03-28T21:33:05Z"
+  }, {
+    "id": 28,
+    "label": "eget semper rutrum nulla",
+    "type": "Boolean",
+    "active": false,
+    "created": "2020-06-28T07:00:56Z"
+  }, {
+    "id": 29,
+    "label": "purus",
+    "type": "Single Select",
+    "active": true,
+    "created": "2020-05-30T23:38:37Z"
+  }, {
+    "id": 30,
+    "label": "eu",
+    "type": "Single Select",
+    "active": true,
+    "created": "2019-09-24T17:54:48Z"
+  }, {
+    "id": 31,
+    "label": "non ligula pellentesque ultrices",
+    "type": "Number",
+    "active": false,
+    "created": "2020-04-15T13:02:01Z"
+  }, {
+    "id": 32,
+    "label": "nisl nunc",
+    "type": "String",
+    "active": true,
+    "created": "2019-11-01T04:42:46Z"
+  }, {
+    "id": 33,
+    "label": "dolor",
+    "type": "Single Select",
+    "active": true,
+    "created": "2019-12-29T23:05:58Z"
+  }, {
+    "id": 34,
+    "label": "consequat in consequat",
+    "type": "Boolean",
+    "active": false,
+    "created": "2020-09-18T04:12:47Z"
+  }, {
+    "id": 35,
+    "label": "ante ipsum primis in",
+    "type": "Number",
+    "active": true,
+    "created": "2019-12-19T17:28:29Z"
+  }, {
+    "id": 36,
+    "label": "lectus suspendisse",
+    "type": "Number",
+    "active": false,
+    "created": "2019-11-24T21:03:38Z"
+  }, {
+    "id": 37,
+    "label": "faucibus orci luctus",
+    "type": "Number",
+    "active": true,
+    "created": "2019-10-03T13:10:32Z"
+  }, {
+    "id": 38,
+    "label": "consequat varius integer ac",
+    "type": "Number",
+    "active": false,
+    "created": "2020-04-05T02:13:49Z"
+  }, {
+    "id": 39,
+    "label": "consequat metus sapien ut",
+    "type": "String",
+    "active": false,
+    "created": "2020-02-02T23:20:30Z"
+  }, {
+    "id": 40,
+    "label": "sed magna at",
+    "type": "Number",
+    "active": false,
+    "created": "2020-03-16T18:45:11Z"
+  }, {
+    "id": 41,
+    "label": "suspendisse",
+    "type": "Single Select",
+    "active": false,
+    "created": "2020-02-27T00:52:20Z"
+  }, {
+    "id": 42,
+    "label": "convallis",
+    "type": "Multi Select",
+    "active": false,
+    "created": "2020-01-04T05:26:54Z"
+  }, {
+    "id": 43,
+    "label": "erat",
+    "type": "String",
+    "active": false,
+    "created": "2019-09-30T03:02:13Z"
+  }, {
+    "id": 44,
+    "label": "eget vulputate ut",
+    "type": "Date Time",
+    "active": false,
+    "created": "2019-09-06T18:15:37Z"
+  }, {
+    "id": 45,
+    "label": "est et",
+    "type": "Number",
+    "active": false,
+    "created": "2020-06-25T10:42:46Z"
+  }];
+
+  export { mockData};

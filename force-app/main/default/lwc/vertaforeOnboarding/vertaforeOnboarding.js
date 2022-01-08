a2873907d68668f9bbe785f6c5fa3146c0d44ce6
Asdilo <asterisk@asteriskloftis.com>
2022-01-08 15:30:50 -0700

    Added buttons, accordion, and other items

--- force-app/main/default/lwc/vertaforeOnboarding/vertaforeOnboarding.js
+++ force-app/main/default/lwc/vertaforeOnboarding/vertaforeOnboarding.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class VertaforeOnboarding extends LightningElement {}
+export default class VertaforeOnboarding extends LightningElement {
+
+    isModalOpen = false;
+    activeSections = ['One'];
+
+    //Modal UI show/hide
+    modalOpen() {
+        this.isModalOpen = true;
+    }
+
+    modalClose() {
+        this.isModalOpen = false;
+    }
+}

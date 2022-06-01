8b57db77e9331e37cb5f62d8c63de96c252812f8
Asterisk Loftis <asterisk@asteriskloftis.com>
2022-06-01 06:14:12 -0700

    Add card Profile list LWC

---
+++ force-app/main/default/lwc/cardProfileList/data.js
@@ -0,0 +1,103 @@
+const profileData = [{
+    "id": 1,
+    "first_name": "Curcio",
+    "last_name": "Yirrell",
+    "full_name": "Curcio Yirrell",
+    "email": "cyirrell0@who.int",
+    "avatar": "https://robohash.org/repellatomnisunde.png",
+    "catch_phrase": "Face to face multimedia conglomeration",
+    "color": "#40e83c",
+    "text": "Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien."
+}, {
+    "id": 2,
+    "first_name": "Rudolph",
+    "last_name": "Capell",
+    "full_name": "Rudolph Capell",
+    "email": "rcapell1@indiegogo.com",
+    "avatar": "https://robohash.org/numquametnobis.png",
+    "catch_phrase": "Sharable 3rd generation matrices",
+    "color": "#50a6bc",
+    "text": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum."
+}, {
+    "id": 3,
+    "first_name": "Elna",
+    "last_name": "Abrams",
+    "full_name": "Elna Abrams",
+    "email": "eabrams2@symantec.com",
+    "avatar": "https://robohash.org/estomniset.png",
+    "catch_phrase": "Upgradable multi-tasking database",
+    "color": "#a088a5",
+    "text": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla."
+}, {
+    "id": 4,
+    "first_name": "Towny",
+    "last_name": "Greenig",
+    "full_name": "Towny Greenig",
+    "email": "tgreenig3@mac.com",
+    "avatar": "https://robohash.org/optioquaefugit.png",
+    "catch_phrase": "Digitized 6th generation utilisation",
+    "color": "#93e311",
+    "text": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo."
+}, {
+    "id": 5,
+    "first_name": "Elmore",
+    "last_name": "Jiggins",
+    "full_name": "Elmore Jiggins",
+    "email": "ejiggins4@ebay.co.uk",
+    "avatar": "https://robohash.org/nobisexpeditaporro.png",
+    "catch_phrase": "Multi-lateral human-resource throughput",
+    "color": "#dc50ea",
+    "text": "Aenean sit amet justo."
+}, {
+    "id": 6,
+    "first_name": "Lindon",
+    "last_name": "Stowte",
+    "full_name": "Lindon Stowte",
+    "email": "lstowte5@berkeley.edu",
+    "avatar": "https://robohash.org/officiacupiditatead.png",
+    "catch_phrase": "User-centric background collaboration",
+    "color": "#88844b",
+    "text": "Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend."
+}, {
+    "id": 7,
+    "first_name": "Edee",
+    "last_name": "Patis",
+    "full_name": "Edee Patis",
+    "email": "epatis6@sciencedaily.com",
+    "avatar": "https://robohash.org/magnamconsequaturtemporibus.png",
+    "catch_phrase": "Multi-layered contextually-based architecture",
+    "color": "#b2dcad",
+    "text": "Morbi a ipsum. Integer a nibh. In quis justo."
+}, {
+    "id": 8,
+    "first_name": "Sayers",
+    "last_name": "Domsalla",
+    "full_name": "Sayers Domsalla",
+    "email": "sdomsalla7@pagesperso-orange.fr",
+    "avatar": "https://robohash.org/etsuntest.png",
+    "catch_phrase": "Innovative discrete software",
+    "color": "#b7b8d0",
+    "text": "Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."
+}, {
+    "id": 9,
+    "first_name": "Hagan",
+    "last_name": "Prime",
+    "full_name": "Hagan Prime",
+    "email": "hprime8@cdbaby.com",
+    "avatar": "https://robohash.org/sapienteconsequatureius.png",
+    "catch_phrase": "Self-enabling neutral forecast",
+    "color": "#d54051",
+    "text": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante."
+}, {
+    "id": 10,
+    "first_name": "Marylin",
+    "last_name": "Marte",
+    "full_name": "Marylin Marte",
+    "email": "mmarte9@cyberchimps.com",
+    "avatar": "https://robohash.org/voluptasveritatisnam.png",
+    "catch_phrase": "Profit-focused interactive extranet",
+    "color": "#f7015c",
+    "text": "Morbi a ipsum. Integer a nibh."
+}];
+
+export { profileData };
